6cc658ee8d5ef8c719c7ab0bdf12f217520fbaf7
llanginger <llanginger@gmail.com>
2016-03-07 12:50:11 -0800

    Completed mem generator

---
+++ canvass.js
@@ -0,0 +1,13 @@
+// <script>
+//   var c = document.querySelector("#c");
+//   var ctx = c.getContext("2d");
+//
+//   var image = new Image();
+//
+//   image.onload = function(){
+//     console.log("Loaded image");
+//     ctx.drawImage(image, 0, 0, c.width, c.height);
+//   };
+//
+//   image.src = "fry.jpg";
+// </script>
